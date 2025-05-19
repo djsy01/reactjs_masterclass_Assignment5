1e35ef879631e4e38bddedb8b7db35f4cea963b5
djsy01 <dlsgh8884@naver.com>
2025-05-19 10:35:34 +0900

    5.11

---
+++ src/api.ts
@@ -0,0 +1,46 @@
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+const BASE_PATH = "https://api.themoviedb.org/3";
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+export function fetchCoins() {
+  return fetch("https://api.coinpaprika.com/v1/coins").then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
+}
---
+++ src/styled.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+import { StringLiteral } from "typescript";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    bgColor: string;
+    textColor: string;
+    accentColor: string;
+  }
+}
