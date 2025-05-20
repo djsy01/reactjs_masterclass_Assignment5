11de0982b926de15a7c54f84b8478a264312db7f
djsy01 <dlsgh8884@naver.com>
2025-05-21 00:08:38 +0900

    6.17

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,35 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atoms";
+import CreateToDo from "./CreateTodo";
+import ToDo from "./Todo";
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
+      <CreateToDo />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
+    </div>
+  );
+}
+
+export default ToDoList;
+
+/*
 import { useRecoilValue } from "recoil";
 import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateTodo";
@@ -38,6 +70,7 @@ function ToDoList() {
 }
 
 export default ToDoList;
+*/
 
 /*
 import { useRecoilValue } from "recoil";
