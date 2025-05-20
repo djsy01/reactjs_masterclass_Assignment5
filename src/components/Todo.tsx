1c365d3fe0fc3631cc8b2c64aa846b8a5725aede
djsy01 <dlsgh8884@naver.com>
2025-05-20 23:59:20 +0900

    6.15

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -41,12 +41,17 @@ import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
-    const setToDos = useSetRecoilState(toDoState);
-    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        const {
-            currentTarget: { name },
-        } = event;
-    };
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name };
+      return oldToDos;
+    });
+  };
 
   return (
     <li>
