02724dc0ed2f4afabf809ae60cf1194d7c41963d
djsy01 <dlsgh8884@naver.com>
2025-05-20 23:17:54 +0900

    6.9

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ import ToDoList from "./TodoList";
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
   /* 여기에 글로벌 스타일 작성 */
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+    margin: 0;
+    padding: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
 `;
 
 function App() {
